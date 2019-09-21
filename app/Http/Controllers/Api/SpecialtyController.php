ee8f257d828a92f92794abcbda1c7c71ad4da70a
Juan Castillo <jach58@hotmail.com>
2019-09-20 21:22:43 -0500

    JWT and API Routes

--- app/Http/Controllers/Api/SpecialtyController.php
+++ app/Http/Controllers/Api/SpecialtyController.php
@@ -8,6 +8,12 @@ use App\Http\Controllers\Controller;
 
 class SpecialtyController extends Controller
 {
+
+    public function index()
+    {
+        return Specialty::all(['id', 'name']);
+    }
+
     public function doctors(Specialty $specialty)
     {
         return $specialty->users()->get(['users.id', 'users.name']);
