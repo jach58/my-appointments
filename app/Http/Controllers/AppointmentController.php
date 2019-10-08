0128a74e728a070040eec11749817c034cbe880c
Juan Castillo <jach58@hotmail.com>
2019-10-07 19:39:49 -0500

    Send FCM to User (cancel, confirm)

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -117,7 +117,10 @@ class AppointmentController extends Controller
         }
 
         $appointment->status = 'Cancelada';
-        $appointment->save();
+        $saved = $appointment->save();
+
+        if($saved)
+        $appointment->patient->sendFCM('Su cita ha sido cancelada.');
 
         $notification= 'La cita se ha cancelado correctamente.';
         return redirect('/appointments')->with(compact('notification'));
@@ -126,7 +129,10 @@ class AppointmentController extends Controller
     public function postConfirm(Appointment $appointment)
     {
         $appointment->status = 'Confirmada';
-        $appointment->save();
+        $saved = $appointment->save();
+
+        if($saved)
+            $appointment->patient->sendFCM('Su cita se ha confirmado!');
 
         $notification= 'La cita se ha confirmado correctamente.';
         return redirect('/appointments')->with(compact('notification'));
